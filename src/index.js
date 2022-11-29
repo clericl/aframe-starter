c83a1e2c1bb01b2174081a8bc4656eb3b05b56fe
Eric L <eric.liang@rosedigital.co>
2022-11-29 16:37:08 -0500

    add static cubemap component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import landingPageComponent from './components/landingPage'
 import loadingSkinnerComponent from './components/loadingSkinner'
 import proximityTriggerComponent from './components/proximityTrigger'
+import staticCubemapComponent from './components/staticCubemap'
 import uiCarouselComponent from './components/uiCarousel'
 
 import './styles/index.scss'
@@ -8,4 +9,5 @@ import './styles/index.scss'
 AFRAME.registerComponent('landing-page', landingPageComponent)
 AFRAME.registerComponent('loading-skinner', loadingSkinnerComponent)
 AFRAME.registerComponent('proximity-trigger', proximityTriggerComponent)
+AFRAME.registerComponent('static-cubemap', staticCubemapComponent)
 AFRAME.registerComponent('ui-carousel', uiCarouselComponent)
